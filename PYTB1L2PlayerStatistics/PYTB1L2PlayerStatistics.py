f154776fd27dc1381c04ac10f556d8efc3202f9c
Abdi <29274@ma-web.nl>
2021-10-06 12:33:51 +0200

    PYTB1L2PlayerStatistics

---
+++ PYTB1L2PlayerStatistics/PYTB1L2PlayerStatistics.py
@@ -0,0 +1,27 @@
+name = "Erwin Henraat"
+jumpforce = int(20)
+Special_power = "teleport"
+speed = 280
+bike = "Honda VFR VTEC"
+Lives_left_just = 1
+ammo = int(121)
+number_of_lasers = float(5)
+battery_power = 90
+level = 9000
+wanted = True
+if wanted  == 1 :
+    print(wanted)
+
+
+print(name)
+print(jumpforce)
+print(Special_power)
+print(speed)
+print(bike)
+print(Lives_left_just)
+print(ammo)
+print(number_of_lasers)
+print(battery_power)
+print(level)
+print(wanted)
+
